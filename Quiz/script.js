b59bf724f1d6ad5a8a2331458848cba3dc1ce136
Osvaldo <torquati.osvaldo@gmail.com>
2020-10-15 23:54:16 -0300

    Finishing
    Atualização, porem com erro ao carregar novas quetões

--- Quiz/script.js
+++ Quiz/script.js
@@ -43,18 +43,22 @@ const quizData = [
        correct: 'c'
        }
 ]
-
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
 const questionEl = document.getElementById("question");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
-
+const submitBtn = document.getElementById("Submit");
 let currentQuiz = 0;
+let score =0;
 
 loadQuiz();
 
 function loadQuiz() {
+    deselectAnswers();
+
     const currentQuizData = quizData [currentQuiz];
 
     questionEl.innerText = currentQuizData.question;
@@ -63,5 +67,46 @@ function loadQuiz() {
     c_text.innerText = currentQuizData.c;
     d_text.innerText = currentQuizData.d;
 
-    currentQuiz++;
+    
 }
+
+function getSelected() {
+    const answerEls = document.querySelectorAll(".answer");
+
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if(answerEl.checked) {
+            return answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked=false;
+    });
+}
+
+submitBtn.addEventListener("click", () => {
+    // Check the Answer
+    const answer = getSelected();
+
+    if (answer) {
+
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            // TODO: Show results
+            quiz.innerHTML = `<h2>Você terminou! Seu resultado foi ${score}/${quizData.lenth} Pegue seu premio</h2>`;
+        }   
+    
+    }  
+});
